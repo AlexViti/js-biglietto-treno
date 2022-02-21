9dc6292245786c316c3032eb5923b4e32b732435
AlexViti <alexviti12@gmail.com>
2022-02-21 17:24:09 +0100

    Add price before discount and discount in output

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 let userAge = prompt("Quanti anni hai?");
 
-while (isNaN(userAge)) {
+while (isNaN(userAge) || userAge === '') {
 	userAge = prompt("Per favore, inserisci un numero per indicare la tua età.");
 }
 
 let km = prompt("Quanti km devi percorrere?");
 
-while (isNaN(km)) {
-	km = prompt("Per favore, inserisci un numero per indicare la distanza che vuoi percorrere.");
+while (isNaN(km) || km === '') {
+	km = prompt("Per favore, inserisci un numero per indicare la distanza che vuoi percorrere in kilometri.");
 }
 
 const priceAtKm = 0.21;
@@ -29,10 +29,10 @@ let finalPrice = (price - discounted).toFixed(2);
 
 let myOutput = document.getElementById("my-output");
 
-myOutput.innerHTML = `Il prezzo del tuo biglietto è: ${finalPrice} €`;
+myOutput.innerHTML = `Hai scelto di viaggiare per ${km} km. Il prezzo del tuo biglietto è: <strong>${finalPrice} €</strong>`;
 
 if (userAge >= 18  && userAge < 65 ) {
 
 } else {
-	myOutput.innerHTML += ` <div> Grazie allo sconto hai risparmiato ${discounted.toFixed(2)} € </div>`;
+	myOutput.innerHTML += `<div>Il prezzo iniziale era di <span style="text-decoration: line-through;">${price.toFixed(2)} €</span>.	Grazie allo sconto hai risparmiato <em style="color:red;">${discounted.toFixed(2)} €</em></div>`;
 }
