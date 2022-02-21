b0dcc1a826153025fe9313d1adb0b6f5306235f8
AlexViti <alexviti12@gmail.com>
2022-02-21 16:54:26 +0100

    Fix and tweaks

--- js/script.js
+++ js/script.js
@@ -6,17 +6,24 @@ let price = (priceAtKm * km);
 
 let discount = 0;
 
-if (userAge > 40) {
+if (userAge >= 65) {
 	discount = 0.4;
-} else if (userAge < 20) {
+} else if (userAge < 18) {
 	discount = 0.2;
 } else {
-	discount = 0;21
-
+	discount = 0;
 }
 
 let discounted = (price * discount);
 
 let finalPrice = (price - discounted).toFixed(2);
 
-document.getElementById("my_output").innerHTML = `Il prezzo del tuo biglietto è: ${finalPrice}` €;
+let myOutput = document.getElementById("my-output");
+
+myOutput.innerHTML = `Il prezzo del tuo biglietto è: ${finalPrice} €`;
+
+if (userAge >= 18  && userAge < 65 ) {
+
+} else {
+	myOutput.innerHTML += ` <div> Grazie allo sconto hai risparmiato ${discounted.toFixed(2)} € </div>`;
+}
